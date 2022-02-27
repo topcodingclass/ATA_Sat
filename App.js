3a31e8985232c8d23b9239a50249a14f8d9171cc
Paul Hwang <promised23@hotmail.com>
2022-02-26 16:46:48 -0800

    Main page work

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
+import React from 'react'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const DATA = [
+  {
+    id:1,
+    title:'Leo the Lion',
+    materials: 'plastic plate, yellow lanyard,beige paint, white pipe cleaner',
+    src: require('./assets/1.png')
+  },
+  {
+    id:2,
+    title: 'Sing Sing Microphone',
+    materials: 'Aluminum Foil, Origami, Tennis Ball (any type of circular ball shape), used toilet paper roll, pipe cleaner',
+    src: require('./assets/2.png')
+  }
+]
+
+const App = () => {
+  const renderItem = ({item, index}) => (
+    <View style={{borderWidth:1, borderColor:'gray', borderRadius:15, width:170, height:200, margin:20, justifyContent:'center', alignItems:'center'}}>
+      <Image style={{width:140, height:160, borderRadius:15}} source= {item.src} />
+      <Text>{item.title}</Text>
     </View>
-  );
+  )
+  return (
+    <View>
+      {/* Logo  */}
+      <View style={{marginTop:20, paddingTop:30, paddingHorizontal:5}}>
+      <Image
+        style={{height:170, width:'100%'}}
+        source={require('./assets/ATALogo.png')}
+      />
+      </View>
+      {/* List */}
+      <View style={{alignItems:'center'}}>
+        <FlatList data = {DATA} renderItem={renderItem} numColumns={2} />
+      </View>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
